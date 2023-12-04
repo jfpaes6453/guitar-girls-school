50fd4e50790d33a50efb131943300b03dfce101a
Gabi <149382614+jfpaes6453@users.noreply.github.com>
2023-12-04 09:47:21 +0100

    update menu hamburguesa (#27)
    * archivos JS Zohra y Steph
    
    * .
    
    * menu hamburguesa arreglado

--- src/javascript/stringsVibration.js
+++ src/javascript/stringsVibration.js
@@ -1,13 +1,19 @@
 // funciones de vibración de las cuerdas de la guitarra
 
+// Obtener todas las cuerdas
 const strings = document.querySelectorAll('.string');
 
+// Función para hacer vibrar la cuerda (div.string)
 function vibrateString(string) {
-  string.classList.add('vibrating'); 
+  string.classList.add('vibrating'); // Aplicar la clase que tiene la animación
+
+  // Eliminar la clase después de un tiempo para detener la vibración
   setTimeout(() => {
     string.classList.remove('vibrating');
   }, 300);
 }
+
+// Agregar un event listener a cada cuerda para activar la vibración al hacer clic
 strings.forEach(string => {
   string.addEventListener('click', () => {
     vibrateString(string);
